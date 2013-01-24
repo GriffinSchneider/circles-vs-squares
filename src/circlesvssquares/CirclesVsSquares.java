00eb9769d39fd8133635d2619aff4427eadab575
Griffin Schneider <griffinschneider@gmail.comm>
2013-01-24 15:51:31 -0500

    Initial commit - new Processing project from Eclipse.

---
+++ src/circlesvssquares/CirclesVsSquares.java
@@ -0,0 +1,17 @@
+package circlesvssquares;
+
+import processing.core.PApplet;
+
+
+public class CirclesVsSquares extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { circlesvssquares.CirclesVsSquares.class.getName() });
+	}
+}
