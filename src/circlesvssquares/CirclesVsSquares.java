4ea3c1ad7a4a2b65fa74b01e2edb6ef02eb1a61e
Griffin Schneider <griffinschneider@gmail.com>
2013-01-24 20:42:20 -0500

    Use 4-space indents like a normal person.

--- src/circlesvssquares/CirclesVsSquares.java
+++ src/circlesvssquares/CirclesVsSquares.java
@@ -5,13 +5,13 @@ import processing.core.PApplet;
 
 public class CirclesVsSquares extends PApplet {
 
-	public void setup() {
-	}
+    public void setup() {
+    }
 
-	public void draw() {
-	}
+    public void draw() {
+    }
 	
-	public static void main(String _args[]) {
-		PApplet.main(new String[] { circlesvssquares.CirclesVsSquares.class.getName() });
-	}
+    public static void main(String _args[]) {
+        PApplet.main(new String[] { circlesvssquares.CirclesVsSquares.class.getName() });
+    }
 }
